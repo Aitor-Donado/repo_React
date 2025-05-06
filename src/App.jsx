2eedd9397f4c36eb4a6b54414f7c8bbd0440e599
Aitor Donado <aitor.donado@gmail.com>
2025-05-06 12:59:01 +0200

    Añadida advertencia multiples tareas urgentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FormTareas from "./components/FormTareas";
 import TablaTareas from "./components/TablaTareas";
 
 function App() {
-  const [tareas, setTareas] = useState([]);
+  const [mostrarAdvertencia, setMostrarAdvertencia] = useState(false);
+  const [tareas, setTareas] = useState(() => {
+    // Cargar tareas desde localStorage si existen
+    const storedTareas = localStorage.getItem("tareas");
+    console.log("Tareas cargadas desde localStorage:", storedTareas);
+    return storedTareas ? JSON.parse(storedTareas) : [];
+  });
+
+  // Guardar tareas en localStorage cada vez que se actualicen
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    if (tareas.filter((tarea) => tarea.prioridad === "alta").length > 3) {
+      setMostrarAdvertencia(true);
+    } else {
+      setMostrarAdvertencia(false);
+    }
+  }, [tareas]);
 
   const agregarTarea = (nuevaTarea) => {
     setTareas([...tareas, { ...nuevaTarea, id: Date.now() }]);
@@ -12,7 +28,6 @@ function App() {
   const borrarTarea = (id) => {
     setTareas(tareas.filter((tarea) => tarea.id !== id));
   };
-
   return (
     <>
       <div className="container">
@@ -20,6 +35,11 @@ function App() {
         <FormTareas onAgregarTareas={agregarTarea} />
         <h2>Tareas pendientes</h2>
         <TablaTareas tareas={tareas} onBorrarTarea={borrarTarea} />
+        {mostrarAdvertencia && (
+          <div id="advertencia" className="alert alert-danger">
+            Tienes demasiadas tareas importantes
+          </div>
+        )}
       </div>
     </>
   );
