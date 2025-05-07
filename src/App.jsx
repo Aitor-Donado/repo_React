00aec9117fbd1e9c34fcaa2b3aa1fdfe06ff267a
Aitor Donado <aitor.donado@gmail.com>
2025-05-07 12:39:03 +0200

    Fin de Cocteles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BuscaCoctel from "./components/BuscaCoctel";
 import MuestraCocteles from "./components/MuestraCocteles";
 
