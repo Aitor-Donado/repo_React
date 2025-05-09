d8db83c9c05c7f0e38efd1b65fa795f9af621cb1
Aitor Donado <aitor.donado@gmail.com>
2025-05-09 12:12:48 +0200

    Capturados los mensajes

--- src/Mensajero.jsx
+++ src/Mensajero.jsx
@@ -1,9 +1,25 @@
-import AuthContext from "./context/AuthContext";
-
+import { useEffect, useState } from "react";
 function Mensajero() {
+  const [mensajes, setMensajes] = useState([]);
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    fetch("https://aitor.alwaysdata.net/listado")
+      .then((response) => response.json())
+      .then((data) => {
+        setMensajes(data.listado);
+        setCargando(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los mensajes:", error);
+        setCargando(true);
+      });
+  }, []);
+
   return (
     <div className="container">
       <h2>Mensajero</h2>
+      <p>{cargando ? "Cargando..." : mensajes[0].titulo}</p>
     </div>
   );
 }
